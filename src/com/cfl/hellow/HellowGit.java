4f7c41b4651f260f7ebff07b28814f1bff07c9b4
cfl <cfl@handsome>
2017-09-03 23:44:39 +0800

    333

--- src/com/cfl/hellow/HellowGit.java
+++ src/com/cfl/hellow/HellowGit.java
@@ -11,10 +11,10 @@ package com.cfl.hellow;
  *
  */
 public class HellowGit {
-
+	// 1
 	public static void main(String[] args) {
 		System.out.println(2);
-		System.out.println(1);
+		System.out.println(4);
 		//22
 	}
 }
