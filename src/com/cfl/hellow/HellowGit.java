c6435e4aa7f11dba3b9e9e536af155ccca00ce2a
cfl <351382691qq>
2017-09-03 22:13:20 +0800

    test

---
+++ src/com/cfl/hellow/HellowGit.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)HellowGit.java 1.0 2017年9月3日
+ *
+ * Copyright (c) 2016, YUNXI. All rights reserved.
+ * YUNXI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.cfl.hellow;
+
+/**
+ * @author cfl
+ *
+ */
+public class HellowGit {
+
+	public static void main(String[] args) {
+		System.out.println("你好啊");
+	}
+}
