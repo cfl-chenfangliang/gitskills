ea5b25880400e06826d54cd684f5b6c626d95d9d
cfl <cfl@handsome>
2017-09-04 00:19:20 +0800

    new2

--- src/com/cfl/hellow/HellowGit.java
+++ src/com/cfl/hellow/HellowGit.java
@@ -13,7 +13,6 @@ package com.cfl.hellow;
 public class HellowGit {
 	// 1
 	public static void main(String[] args) {
-		System.out.println(2);
-		System.out.println(4);
+		System.out.println("new2");
 	}
 }
